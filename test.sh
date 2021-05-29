ab5b4a32c373dc59286a3147f86fa03ff21dfffe
TA Thanh Dinh <tathanhdinh@gmail.com>
2021-05-29 08:51:57 +0200

    Add the notion of the expression statement

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 cc="zig cc -Wno-unused-command-line-argument"
 asm="nasm -felf64"
-link="ld.gold"
+link="ld.lld --discard-all --strip-all --pie --entry main"
 
 assert() {
 	expected="$1"
@@ -11,7 +11,7 @@ assert() {
 	./out/kleincc "$input" > tmp.asm
 	$asm tmp.asm -o tmp.o
 	# $cc tmp.o -o tmp
-	$link -e main tmp.o -o tmp
+	$link tmp.o -o tmp
 	./tmp
 	actual="$?"
 
@@ -23,49 +23,52 @@ assert() {
 	fi
 }
 
-assert 7 '7;'
-assert 45 '45;'
-assert 5   '3-4+7-1;'
-assert 41  ' 12 + 34 - 5 ;'
-assert 11  ' 34 - 7-16; '
-assert 35  '19 + 8 *2;'
-assert 220 '20 * (5 + 6);'
-assert 4   '(3+5)/2;'
-assert 92 '2 *(5+ 6 * 8 - 7);'
-assert 10 '- - +10;'
-assert 14 '10 -- +4;'
+assert 7 'return 7;'
+assert 45 'return 45;'
+assert 5 'return 3-4+7-1;'
+assert 41 ' return  12 + 34 - 5 ;'
+assert 11 ' return 34 - 7-16; '
+assert 35  'return  19 + 8 *2;'
+assert 220 'return 20 * (5 + 6);'
+assert 4   'return (3  +5)/2;'
+assert 92 'return 2 *(5+ 6 * 8 - 7);'
+assert 10 'return - - +10;'
+assert 14 'return 10 -- +4;'
 
-assert 0 '0==1;'
-assert 1 '42==42;'
-assert 1 '0!=1;'
-assert 0 '42!=42;'
+assert 0 '  return   0==1;'
+assert 1 'return 42==42;'
+assert 1 'return 0!=1;'
+assert 0 'return 42!=42;'
 
-assert 1 '0<1;'
-assert 0 '1<1;'
-assert 0 '2<1;'
-assert 1 '0<=1;'
-assert 1 '1<=1;'
-assert 0 '2<=1;'
+assert 1 '  return 0<1;'
+assert 0 'return 1<1;'
+assert 0 ' return 2<1;'
+assert 1 ' return 0<=1;'
+assert 1 'return 1<=1;'
+assert 0 'return 2<=1;'
 
-assert 1 '1>0;'
-assert 0 '1>1;'
-assert 0 '1>2;'
-assert 1 '1>=0;'
-assert 1 '1>=1;'
-assert 0 '1>=2;'
+assert 1 'return 1>0;'
+assert 0 'return 1>1;'
+assert 0 'return 1>2;'
+assert 1 'return 1>=0;'
+assert 1 'return 1>=1;'
+assert 0 'return 1>=2;'
 
-assert 1 '30 < (24 + 9);'
-assert 1 '37 == (18 + 20 - 1);'
-assert 0 '90 <= (+73 - 22 - 50);'
-assert 0 '42 == (-33 + 51 - 4) ;'
-assert 0 '(19 + 27) > (+28 --18); '
-assert 0 '(33- 44 +++55) > (70-90+130); '
-assert 1 '((1+2+3+4) - (4+3+2+1)) == 0;'
-assert 1 '++3- 4 + 5 > 1 + 1 -+2 ;'
-assert 1 '29- (4 + 2) + (5 -- 3) > 1 + 1 -+2;'
-assert 1 ' 29++- (2 - 7 + 9) -   (12 -- 3) > 1 + 1 -+2  ;'
+assert 1 'return 30 < (24 + 9);'
+assert 1 'return 37 == (18 + 20 - 1);'
+assert 0 'return 90 <= (+73 - 22 - 50);'
+assert 0 'return 42 == (-33 + 51 - 4) ;'
+assert 0 'return (19 + 27) > (+28 --18); '
+assert 0 'return (33- 44 +++55) > (70-90+130); '
+assert 1 'return ((1+2+3+4) - (4+3+2+1)) == 0;'
+assert 1 'return ++3- 4 + 5 > 1 + 1 -+2 ;'
+assert 1 'return 29- (4 + 2) + (5 -- 3) > 1 + 1 -+2;'
+assert 1 'return 29++- (2 - 7 + 9) -   (12 -- 3) > 1 + 1 -+2  ;'
 
-assert 3 '1; 2; 3;'
-assert 7 '11 - 9; 25; +6-7; 1+3+8-5;'
+assert 1 'return 1; 2; 3;'
+assert 2 'return 11 - 9; 25; +6-7; 1+3+8-5;'
+
+assert 20 'return 20; 3; 9 + 12;'
+assert 7 '12 -+5; return 3 + 4;'
 
 echo ok
